31f690b53482fdbd1af9ba994211bc2f16804f3d
ChristianParnell <81597198+ChristianParnell@users.noreply.github.com>
2026-05-14 09:48:24 +1200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,16 +4,15 @@ window.FLATWISE_CONFIG = {
   minBoundaryZoom: 16,
   maxBoundaryZoom: 19,
   mapBounds: [[-47.8, 165.5], [-33.8, 179.5]],
+
   osmTileUrl: "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
 
-  // Public LINZ ArcGIS REST services used directly by this static GitHub Pages prototype.
   linzParcelsEndpoint: "https://services.arcgis.com/xdsHIIxuCWByZiCB/arcgis/rest/services/LINZ_NZ_Primary_Parcels/FeatureServer/0/query",
   linzBuildingsEndpoint: "https://services.arcgis.com/xdsHIIxuCWByZiCB/arcgis/rest/services/LINZ_NZ_Building_Outlines/FeatureServer/0/query",
 
-  // Optional image support. Keep false unless you add a restricted browser key for your own domain.
+  // Optional: set to true and add a restricted browser key if you want Street View thumbnails.
   enableGoogleStreetView: false,
   googleStreetViewApiKey: "",
 
-  // Keep false for privacy. If true, parcel appellation/address-like fields may be shown when LINZ returns them.
   showExactAddress: false
 };
